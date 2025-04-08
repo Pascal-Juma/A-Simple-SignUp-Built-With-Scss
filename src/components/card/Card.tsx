2417a8741e5c7c12946605e73e412c0a8f7d63be
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-08 23:55:12 +0300

    Build the card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,16 +1,14 @@
 import './Card.css'
-import music from '../../assets/muziki.jpg'
 
 function Card() {
   return (
     <>
       <div className="card-container">
         <div className="image-wrapper">
-            <img src={music} alt="music image" />
         </div>
         <div className="card-content">
             <p className="card-paragragh">
-                Create an account and start enjoying your top music <span className="paragragh-stylings">For free</span>
+                Create an account and start enjoying your top music <br /><span className="paragragh-stylings">For free</span>
             </p>
         </div>
       </div>
