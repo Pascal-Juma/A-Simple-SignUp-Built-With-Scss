5f018a15e7d9cece563709a67c043db8545b6856
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-08 21:40:13 +0300

    Set version control for the project

---
+++ src/components/sign-up/Register.tsx
@@ -0,0 +1,10 @@
+
+function Register() {
+  return (
+    <div>
+      <h1>please login</h1>
+    </div>
+  )
+}
+
+export default Register
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Card from '../../components/card/Card'
+import Register from '../../components/sign-up/Register'    
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div className="home-container">
+      <Card />
+      <Register />
+    </div>
+    </>
+  )
+}
+
+export default Home
